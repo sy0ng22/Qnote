d71022ecec400d96d0a8dae3b9395d49d474ecbe
soyoung <soyoung@qnote.app>
2025-10-12 21:36:34 +0900

    Update design: Add monthly calendar, redesign buttons, unify styles

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,9 @@ export default defineConfig({
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
+        daily: resolve(__dirname, 'daily.html'),
         card: resolve(__dirname, 'card.html'),
+        monthly: resolve(__dirname, 'monthly.html'),
         week: resolve(__dirname, 'week.html'),
         cardView: resolve(__dirname, 'card-view.html'),
       },
